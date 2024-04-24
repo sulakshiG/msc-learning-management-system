da20a393bf1a2457cb6e1b4b3070e321c3a4a705
sameera910409 <sameera910409@bitbucket.org>
2024-04-24 10:48:12 +0530

    further commit on repositories and exceptions

---
+++ src/main/java/com/springbootbackendmsclms/repository/GradeRepository.java
@@ -0,0 +1,8 @@
+package com.springbootbackendmsclms.repository;
+
+import com.springbootbackendmsclms.entity.Grade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GradeRepository extends JpaRepository<Grade, Object> {
+
+}
